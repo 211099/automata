cf804ee8d87b5587bb04d3ab557a95b3cc4f7ae4
211099 <211099@ids.upchiapas.edu.mx>
2023-01-29 22:14:53 -0600

    ya valida si no es valida y guarda las cadenas que son validas

--- tareas/trancisiones.py
+++ tareas/trancisiones.py
@@ -14,26 +14,52 @@ trancisiones = [#ES,EL,ES
 
 
 fecha = "31"
+
 estados_finales = [4,5,7]
 
-# Ejemplo con un argumento
 
+#la funcion lee cual es el camino que tomo para llegar al resultado 
 def leer_trancisiones():
     global estado_actual
+    global auxiliar_str
+    global cadenas_recopiladas
+    cadenas_recopiladas = []
+    guardar_o_no_guardar = True 
+    auxiliar_str = ""
     estado_actual = 1
-
-
     print("estado actual: ", estado_actual)
-    
-    if len(fecha) == 2:
-        for elemento in fecha:
+
+    for elemento in fecha:
+        if validar_estado_trancision():
             for i in range(6):
-                
                 if estado_actual == trancisiones[i][0]:
                     if elemento in trancisiones[i][1]:
                         estado_actual = trancisiones[i][2]
                         print("estado actual: ", estado_actual)
+                        auxiliar_str = auxiliar_str +  elemento
                         i = 6
+
+        else:
+            print("cadena no valida ")
+            guardar_o_no_guardar = False
+
+    if guardar_o_no_guardar == True:
+        print("si")
+        cadenas_recopiladas.append(auxiliar_str)
+       
+                    
+                       
+#la funcion valida si existe alguna trancision que salga del estado actual
+def validar_estado_trancision():
+     for i in  range(6):
+        if estado_actual == trancisiones[i][0]:
+            return True
+
+
+# def prueba():
+#     if validar_estado_trancision() == False and validar_si_Es_estado_final() ==  
+
+
     
 
                
@@ -43,10 +69,7 @@ def leer_trancisiones():
             
 
    
-# for i in range(10, 33):
-#     fecha_int = int(fecha)
-#     auxiliar = fecha_int + 1
-#     auxiliar_str = str(auxiliar)
-#     fecha = auxiliar_str
-#     print(fecha)
+
 leer_trancisiones()
+print(cadenas_recopiladas)
+
